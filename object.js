7190aa867de91db0b5e5ca529c1a1f298efb593e
second <phamdangtrungnghia@gmail.com>
2025-12-02 16:51:05 +0300

    basic about object 91 - 92

---
+++ object.js
@@ -0,0 +1,18 @@
+const student = {
+    // key : value
+    name: "Nghia",
+    "first-name": "Pham", // boi vi co chua dau - nen phai de trong ""
+    age: 22,
+    male: true,
+    hi: function() {
+        console.log("Nghia say hello");
+    }
+};
+
+// console.log(student.name);
+// console.log(student["first-name"]); // boi vi co chua dau -
+student.name = "Hieu";
+
+// delete field from object
+// delete student["first-name"];
+
