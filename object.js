46d06aee82e87e3c71c241cd134c6e98fee43a40
second <phamdangtrungnghia@gmail.com>
2025-12-02 16:52:23 +0300

    clone object 100-101

--- object.js
+++ object.js
@@ -21,3 +21,25 @@ student.name = "Hieu";
 //     console.log(`${key} : ${student[key]}`);
 // }
 
+// clone object 
+const user = {
+	userName : "Nghia",
+	school: {
+		name: "ITMO",
+		class: {
+			name: "P3321",
+			course: 3
+		}
+	}
+};
+// console.log(user);
+
+const newUser = {...user};  // spread operator
+const newUser2 = Object.assign({}, user);  // assign method
+const newUser3 = JSON.parse(JSON.stringify(user)); // Object(JSON) -> string -> Object
+newUser.password = 1234;   // add field
+newUser2.password = 12345; // add field
+
+console.log(newUser);   
+console.log(newUser2); 
+console.log(newUser3);
